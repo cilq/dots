e24423ba607f7d399e0132fb5294b57a286f3175
André Oswald <andre.oswald@clondiag.com>
2015-11-15 16:58:54 +0100

    - modular zsh configuration - local config files moved to separate repository

---
+++ .zsh/history.zsh
@@ -0,0 +1,9 @@
+HISTSIZE=12000
+SAVEHIST=10000
+HISTFILE=~/.history
+setopt share_history
+setopt append_history
+setopt extended_history
+setopt hist_ignore_all_dups
+setopt hist_no_store
+setopt hist_ignore_space
---
+++ .zsh/prompt.zsh
@@ -0,0 +1,20 @@
+autoload colors && colors
+
+autoload -Uz vcs_info
+zstyle ':vcs_info:*' enable git svn
+zstyle ':vcs_info:*:prompt:*' check-for-changes true
+
+setopt prompt_subst
+autoload -U promptinit
+promptinit
+
+zstyle ':vcs_info:*' check-for-changes true
+zstyle ':vcs_info:*' unstagedstr '*'   # display this when there are unstaged changes
+zstyle ':vcs_info:*' stagedstr '+'  # display this when there are staged changes
+zstyle ':vcs_info:*' actionformats '[%b%|%a%c%u%]%f'
+zstyle ':vcs_info:*' formats ':%b%c%u%f'
+
+zstyle ':vcs_info:(sv[nk]|bzr):*' branchformat '%b%F{1}:%F{3}%r'
+precmd () { vcs_info }
+
+PROMPT='[%n@%m %~${vcs_info_msg_0_}] %{$reset_color%}'
